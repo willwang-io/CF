84599c64b132bbca8748b7a77dc766d1be45db06
willwang <jiayewang65@gmail.com>
2023-12-10 22:39:06 -0600

    Check

---
+++ A_Cinema_Line.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define EACH(x, a) for (auto& x: a)
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+
+#define nl '\n'
+#define vt vector
+typedef long long int ll;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+const int d4i[4]={-1,0,1,0}, d4j[4]={0,1,0,-1};
+const int d8i[8]={-1,-1,0,1,1,1,0,-1}, d8j[8]={0,1,1,1,0,-1,-1,-1};
+
+template <class T> void printv (const vector<T> &V) {EACH(v, V) cout << v << " ";cout << nl;}
+template<class T> bool umin(T& a, const T& b) {return b<a?a=b, 1:0;}
+template<class T> bool umax(T& a, const T& b) {return a<b?a=b, 1:0;}
+void yes() {cout << "YES\n";}
+void no() {cout << "NO\n";}
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+
+vt<vt<int>> adj;
+vt<int> seen;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vt<int> A(n);
+    EACH(a, A) cin >> a;
+    vt<int> cnt = {0, 0, 0, 0};
+    EACH(a, A) {
+        if (a == 25) {
+            ++cnt[0];
+        } else if (a == 50) {
+            if (!cnt[0]) {
+                no();
+                return 0;
+            }
+            --cnt[0];
+            ++cnt[1];
+        } else if (a == 100) {
+            if (cnt[1] && cnt[0]) {
+                --cnt[1], --cnt[0];
+                ++cnt[2];
+            } else if (cnt[0] >= 3) {
+                cnt[0] -= 3;
+                ++cnt[2];
+            } else {
+                no();
+                return 0;
+            }
+        } 
+    }
+    yes();
+}
