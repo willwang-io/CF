407c335748dc6b43e56b88fa7e442b4528d9d095
willwang <jiayewang65@gmail.com>
2024-01-14 23:05:22 -0600

    CF practice

---
+++ A_Mike_and_palindrome.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+
+#define nl '\n'
+#define vt vector
+typedef long long int ll;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+const int d4i[4]={-1,0,1,0}, d4j[4]={0,1,0,-1};
+const int d8i[8]={-1,-1,0,1,1,1,0,-1}, d8j[8]={0,1,1,1,0,-1,-1,-1};
+
+template <class T> void printv (const vector<T> &V) {for(auto& v:  V) cout << v << " ";cout << nl;}
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+
+vt<vt<int>> adj;
+vt<int> seen;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    int i = 0, j = s.size() - 1, cnt = 0;
+    while (i < j) {
+        if (s[i] != s[j]) {
+            ++cnt;
+        }
+        ++i, --j;
+    }
+    if (!cnt)
+        cout << (s.size() % 2 == 1 ? "YES" : "NO") << nl;
+    else {
+        cout << (cnt == 1 ? "YES" : "NO") << nl;
+    }
+}
