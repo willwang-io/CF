5f447a12e2da8f826db4759a7df8eb5ac73bb4a3
willwang <jiayewang65@gmail.com>
2024-05-08 22:23:35 -0600

    CF practice

---
+++ A_Two_Friends.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <cmath>
+#include <cstring>
+
+typedef long long ll;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+        --a[i];
+    }
+    for (int i = 0; i < n - 1; ++i) {
+        if (a[a[i]] == i) {
+            std::cout << 2 << '\n';
+            return;
+        }
+    }
+    std::cout << 3 << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    std::cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        solve();
+    }
+}
