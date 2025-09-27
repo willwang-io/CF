eedb96a457e2bf2901838e51b205a43cb98bd47e
willwang-io <brill.13.koi@icloud.com>
2025-09-26 23:12:20 -0600

    CF Practice

---
+++ B_2149.rs
@@ -0,0 +1,32 @@
+fn solve() {
+    let n: usize = read();
+    let mut a: Vec<i32> = (0..n).map(|_| read()).collect();
+    a.sort();
+    let mut ans = i32::MIN;
+    for i in (1..n).step_by(2) {
+        ans = ans.max((a[i] - a[i - 1]).abs());
+    }
+    println!("{ans}");
+}
+
+fn main() {
+    let t: usize = read();
+    for _ in 0..t {
+        solve();
+    }
+}
+
+thread_local! {
+    pub static INPUT: std::cell::RefCell<std::str::SplitAsciiWhitespace<'static>> = std::cell::RefCell::<std::str::SplitAsciiWhitespace<'static>>::new({
+        let mut input = String::new();
+        std::io::Read::read_to_string(&mut std::io::stdin(), &mut input).unwrap();
+        Box::leak(input.into_boxed_str()).split_ascii_whitespace()
+    });
+}
+ 
+pub fn read<T: std::str::FromStr>() -> T
+where
+    T::Err: std::fmt::Debug,
+{
+    INPUT.with(|input| input.borrow_mut().next().unwrap().parse().unwrap())
+}
