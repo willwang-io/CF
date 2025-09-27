dbf25c5f9c60817ce1c244b88beefc7f3063af31
willwang-io <brill.13.koi@icloud.com>
2025-09-26 23:02:08 -0600

    CF Practice

---
+++ src/C_1791.rs
@@ -0,0 +1,42 @@
+fn solve() {
+    let n: usize = read();
+    let s: String = read();
+    let s = s.as_bytes();
+    let mut i = 0;
+    let mut j = s.len() - 1;
+    while i < j {
+        if s[i] != s[j] {
+            i += 1;
+            j -= 1;
+        } else {
+            break;
+        }
+    } 
+    if j < i {
+        println!("0");
+    } else {
+        println!("{}", j - i + 1);
+    }
+}
+
+fn main() {
+    let t: usize = read();
+    for _ in 0..t {
+        solve();
+    }
+}
+
+thread_local! {
+    pub static INPUT: std::cell::RefCell<std::str::SplitAsciiWhitespace<'static>> = std::cell::RefCell::<std::str::SplitAsciiWhitespace<'static>>::new({
+        let mut input = String::new();
+        std::io::Read::read_to_string(&mut std::io::stdin(), &mut input).unwrap();
+        Box::leak(input.into_boxed_str()).split_ascii_whitespace()
+    });
+}
+ 
+pub fn read<T: std::str::FromStr>() -> T
+where
+    T::Err: std::fmt::Debug,
+{
+    INPUT.with(|input| input.borrow_mut().next().unwrap().parse().unwrap())
+}
